9ab0318aeaa88091b3a78b94a08ef1794a91fb5c
thyall <thyall96@gmail.com>
2019-12-01 23:17:17 -0300

    dasd

---
+++ build/CMakeFiles/glife.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/glife.dir/src/func_gamelife.cpp.o"
+  "CMakeFiles/glife.dir/src/main.cpp.o"
+  "glife.pdb"
+  "glife"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/glife.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
